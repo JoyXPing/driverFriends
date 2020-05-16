6d5eed2f29747a5592cef2f6f05b7a5bc944a610
羽轻浮世殇 <zdhunter@foxmail.com>
2020-05-16 16:13:09 +0800

    test

--- src/components/boss/BossTask.vue
+++ src/components/boss/BossTask.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        BossTask
+        <get-task-list />
     </div>
 </template>
 
 <script>
+import getTaskList from '@/components/boss/task/getTaskList.vue';
     export default {
-        
+        components: {
+            getTaskList
+        }
     }
 </script>
 
